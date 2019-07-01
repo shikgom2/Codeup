d5e0e9b87c1a0cbc3b9086384f7b4c3342d2c05a
Kim Yong Seok <shikgom2@naver.com>
2019-07-01 14:39:34 +0900

    1403

---
+++ 1400/1403.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	int a[100], k, i;
+	
+	scanf("%d", &k);
+	for(i = 0; i < k; i++)
+		scanf("%d", &a[i]);
+
+	for(i = 0; i < k; i++)
+		printf("%d\n", a[i]);
+	for(i = 0; i < k; i++)
+	
+		printf("%d\n", a[i]);
+	return 0;
+}
