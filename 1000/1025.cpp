61f658b716a0744b94d2c18c5a542dc101c976dd
Kim Yong Seok <shikgom2@naver.com>
2019-07-01 11:20:44 +0900

    1025

---
+++ 1000/1025.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, c, d, e;
+    scanf("%1d%1d%1d%1d%1d", &a, &b, &c, &d, &e);
+    printf("[%d]\n", a*10000);
+    printf("[%d]\n", b*1000);
+    printf("[%d]\n", c*100);
+    printf("[%d]\n", d*10);
+    printf("[%d]\n", e);
+    return 0;
+}
