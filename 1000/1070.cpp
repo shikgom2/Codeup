c46c6560665d9a6af2b6d28df684e495e0b47f9c
Kim Yong Seok <shikgom2@naver.com>
2019-07-01 11:43:38 +0900

    1070

---
+++ 1000/1070.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    char a;
+    scanf("%d",&a);
+    switch(a)
+    {
+        case 12:
+        case 1:
+        case 2:printf("winter");break;
+        case 3:
+        case 4:
+        case 5:printf("spring");break;
+        case 6:
+        case 7:
+        case 8:printf("summer");break;
+        case 9:
+        case 10:
+        case 11:printf("fall");break;
+         
+    }
+ 
+    return 0;
+}
