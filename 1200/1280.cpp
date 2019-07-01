acbbbe1ff944f23a5e389711e54538e9a57f89a6
Kim Yong Seok <shikgom2@naver.com>
2019-07-01 14:23:30 +0900

    1280

---
+++ 1200/1280.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+ 
+int main()
+{
+    int i, a, b, min, max, sum = 0;
+ 
+    scanf("%d %d", &a, &b);
+    if (a>=b) { 
+        max=a; 
+        min=b;
+    }
+    else
+    {
+        max=b;
+        min=a;
+    }
+ 
+    for(i = min; i <= max; i++)
+    {
+        if(i%2 == 1)
+        {
+            sum += i;
+            printf("+%d",i);
+        }
+        else
+        {
+            sum -= i;
+            printf("-%d",i);
+        }
+    }
+ 
+    printf("=%d", sum);
+    return 0;
+}
