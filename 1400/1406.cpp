7bc4f80f9247adcf441ce992ca813f9499108cc0
Kim Yong Seok <shikgom2@naver.com>
2019-07-01 14:40:04 +0900

    1406

---
+++ 1400/1406.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main ()
+{
+    char s[200]; 
+    scanf("%s",s);
+    if (s[0]=='l' && s[1]=='o' && s[2]=='v' && s[3]=='e' && s[4]=='\0') 
+    	printf("I love you.");
+    return 0;
+}
