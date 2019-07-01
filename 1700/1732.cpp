cf487334c289d5724ad025cc4ead78327212f83a
Kim Yong Seok <shikgom2@naver.com>
2019-07-01 15:58:51 +0900

    1732

---
+++ 1700/1732.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	int i,j;
+	
+	scanf("%d %d",&i,&j);
+	
+	printf("%.1f\n",(double)i/(double)j);
+	printf("%d\n",i/j);
+	printf("%.0f",pow(i,j));
+	
+	return 0;
+}
+v
