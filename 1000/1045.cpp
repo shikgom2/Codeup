a43ba294e0ef3036d28f7df872cfd6f274d259e9
Kim Yong Seok <shikgom2@naver.com>
2019-07-01 11:35:16 +0900

    1045

---
+++ 1000/1045.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d\n", a+b);
+    printf("%d\n", a-b);
+    printf("%d\n", a*b);
+    printf("%d\n", a/b);
+    printf("%d\n", a%b);
+    printf("%.2lf", (double)a/b);
+    return 0;
+}
