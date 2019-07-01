d8a0843a40003d196ddfa2b6d39b70c3ba9c91b2
Kim Yong Seok <shikgom2@naver.com>
2019-07-01 15:00:16 +0900

    1504

---
+++ 1500/1504.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+ 
+int main() 
+{
+    int n, i, j, arr[105][105], count = 1;
+    
+    scanf("%d",&n);
+    for (i=0;i<n;i++) {
+        if (i % 2 == 0) {
+            for (j=0;j<n;j++) {
+                arr[j][i] = count;
+                count++;
+            }
+        }
+        else {
+            for (j=n-1;j>=0;j--) {
+                arr[j][i] = count;
+                count++;
+            }
+        }
+    }
+    for (i=0;i<n;i++) {
+        for (j=0;j<n;j++) {
+            printf("%d ",arr[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
