e2ff38fc3990e7d95c3d01a0fc9e5226be942eae
Kim Yong Seok <shikgom2@naver.com>
2019-07-03 10:08:59 +0900

    2008

---
+++ 2000/2008.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>  
+
+int main()  
+{
+	int a[100], i, n, as = 0, des = 0, mix = 0;
+
+	scanf("%d\n%d", &n, &a[0]);
+	for(i = 1; i < n; i++)
+	{
+		scanf("%d", &a[i]);
+		if(a[i-1] < a[i]) as = 1;
+		if(a[i-1] > a[i]) des = 1;
+	}
+
+	if(as==des) printf("섞임");
+	else if(as==1) printf("오름차순");
+	else printf("내림차순");
+	return 0;
+}
