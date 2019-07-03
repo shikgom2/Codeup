7c3b7f050880099f2c17901b87fd13b9b7d6d705
Kim Yong Seok <shikgom2@naver.com>
2019-07-03 15:43:13 +0900

    2414

---
+++ 2400/2414.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args){
+		Scanner sc = new Scanner (System.in);
+		
+		int n = sc.nextInt();
+		sc.nextLine();
+		
+		String temp = "";
+		String friendName = "";
+		
+		ArrayList<String> list = new ArrayList<String>();
+		
+		double result = 0;
+		
+		for ( int i = 0; i < n; i++ ){
+			temp = sc.nextLine();
+			list.add(temp);
+		}
+		
+		friendName = sc.nextLine();
+
+		for ( int i = 0; i < n; i++ ){
+			if(friendName.equals(list.get(i).split(",")[0])){
+				System.out.println(list.get(i).split(",")[2]);
+			}
+		}
+	}
+}
