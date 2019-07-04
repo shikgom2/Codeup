439417cd3eaa4d0bc0ad8eacefa4bab53414cc09
Kim Yong Seok <shikgom2@naver.com>
2019-07-04 18:00:19 +0900

    2625

---
+++ 2600/2625.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+ 
+int main() {
+    int n, i;
+    long long t = 0;
+ 
+    scanf("%d", &n);
+ 
+    for (i = n / 3 + (n % 3 != 0);i < n / 2 + n % 2;i++)
+        t += i - ((n - i) / 2 + (n - i) % 2) + 1;
+ 
+    printf("%lld", t);
+ 
+}
